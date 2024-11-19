ac4d1cf9b65666097fe40767866bc6177be7e229
Adossi Fred William <f.adossi@alustudent.com>
2024-11-19 21:34:28 +0200

    Change welcom screen

--- pld_2.py
+++ pld_2.py
@@ -1,8 +1,8 @@
 def display_welcome_screen():
     """Display the welcome screen and rules."""
-    print("=" * 40)
+    print("=" * 45)
     print("\tPython Knowledge Assessment")
-    print("=" * 40)
+    print("=" * 45)
     print("\nRules & Guidelines:")
     print("- Each question has 4 multiple-choice options")
     print("- Select your answer by typing 1, 2, 3, or 4")
